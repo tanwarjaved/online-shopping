b6d5cd894c4e8ad3b981cb8f3f40981ef0d3584b
tanwarjaved <tanwarjaved@yahoo.com>
2023-10-19 13:07:09 -0700

    Initial commit with empty structure

---
+++ shoppingbackend/src/main/java/net/jt/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package net.jt.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
